e4e59fb1f5401c91e119f5b719e8c685d82e77cc
Wen Junhua <wenjh1998@gmail.com>
2023-10-17 00:07:24 +0800

    tests: add benchmark

--- ReadMe.md
+++ ReadMe.md
@@ -2,23 +2,50 @@
 
 A mini validation library in Pure Go.
 
-## Usage
+Please create an issue if you have any suggestions or find any bugs / problems.
+
+## Example
 
 ```go
+package main
 
 import (
-    validate "github.com/Jh123x/go-validate"
+    "github.com/Jh123x/go-validate/options"
+    "github.com/Jh123x/go-validate/validator"
 )
 
 func main(){
-    validator := NewLazyValidator()
-    err := validator.WithOptions(
-        IsNotEmpty("").WithError(fmt.Errorf("empty string")),               // Fails and returns error.
-        IsLength([]string{}, 0, 3).WithError(fmt.Errorf("empty string")),   // Will not be evaluated.
-    ).Validate()
+    lazyValidator := validator.NewLazyValidator()
+	err := lazyValidator.WithOptions(
+		options.IsNotEmpty("").WithError(fmt.Errorf("empty string")),             // Fails and returns error.
+		options.IsLength([]string{}, 0, 3).WithError(fmt.Errorf("empty string")), // Will not be evaluated.
+	).Validate()
     if err != nil {
         // handle error
         ...
     }
 }
+```
+
+## Benchmark
+
+This is the test results on my PC.
+
+```
+goos: windows
+goarch: amd64
+pkg: github.com/Jh123x/go-validate/validator
+cpu: AMD Ryzen 5 7600 6-Core Processor
+
+BenchmarkData/with_error_in_message_for_TestLazyValidator-12         	 2721825	       431.0 ns/op	     280 B/op	       9 allocs/op
+BenchmarkData/with_error_in_message_for_TestOzzo-12                  	 1291412	       928.2 ns/op	    1144 B/op	      20 allocs/op
+BenchmarkData/with_error_in_extras_for_TestLazyValidator-12          	 2808309	       430.3 ns/op	     280 B/op	       9 allocs/op
+BenchmarkData/with_error_in_extras_for_TestOzzo-12                   	 1307173	       913.8 ns/op	    1144 B/op	      20 allocs/op
+BenchmarkData/no_errors_for_TestOzzo-12                              	 1448179	       817.0 ns/op	     840 B/op	      18 allocs/op
+BenchmarkData/no_errors_for_TestLazyValidator-12                     	 2804840	       430.8 ns/op	     280 B/op	       9 allocs/op
+BenchmarkData/with_error_in_code_for_TestLazyValidator-12            	 2858064	       418.4 ns/op	     280 B/op	       9 allocs/op
+BenchmarkData/with_error_in_code_for_TestOzzo-12                     	 1288452	       924.6 ns/op	    1144 B/op	      20 allocs/op
+PASS
+
+ok  	github.com/Jh123x/go-validate/validator	15.144s
 ```
--- validator/validator_test.go
+++ validator/validator_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/Jh123x/go-validate/options"
 	"github.com/Jh123x/go-validate/ttypes"
+	validation "github.com/go-ozzo/ozzo-validation"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -45,6 +46,7 @@ func TestLazyValidator_Caching(t *testing.T) {
 	assert.Equal(t, errTest, validator2.Validate())
 }
 
+// TestLazyValidator_ReadMeExample tests the example in the README.
 func TestLazyValidator_ReadMeExample(t *testing.T) {
 	validator := NewLazyValidator()
 	err := validator.WithOptions(
@@ -52,4 +54,89 @@ func TestLazyValidator_ReadMeExample(t *testing.T) {
 		options.IsLength([]string{}, 0, 3).WithError(fmt.Errorf("empty string")), // Will not be evaluated.
 	).Validate()
 	assert.Equal(t, fmt.Errorf("empty string"), err)
+}
+
+/* Test Scenario for Benchmarking */
+type Response struct {
+	Code    int            // Must be non-zero.
+	Message string         // Must be non-empty.
+	Extras  map[string]any // Must be non-nil.
+}
+
+type testFn func(*Response) error
+
+func benchmarkValidator(b *testing.B, response *Response, validateFn testFn, hasErr bool) {
+	for i := 0; i < b.N; i++ {
+		err := validateFn(response)
+		assert.Equal(b, hasErr, err != nil, err)
+	}
+}
+
+func validateResponseLazy(resp *Response) error {
+	return NewLazyValidator().WithOptions(
+		options.IsNotEmpty(resp.Code),
+		options.IsNotEmpty(resp.Message),
+		options.WithRequire(func() bool { return resp.Extras != nil }, errTest),
+	).Validate()
+}
+
+func validateResponseOzzo(resp *Response) error {
+	return validation.ValidateStruct(
+		resp,
+		validation.Field(&resp.Code, validation.NilOrNotEmpty),
+		validation.Field(&resp.Message, validation.NilOrNotEmpty),
+		validation.Field(&resp.Extras, validation.NotNil),
+	)
+}
+
+func BenchmarkData(b *testing.B) {
+	algorithms := map[string]testFn{
+		"TestLazyValidator": validateResponseLazy,
+		"TestOzzo":          validateResponseOzzo,
+	}
+	tests := map[string]struct {
+		resp   Response
+		hasErr bool
+	}{
+		"no errors": {
+			resp: Response{
+				Code:    200,
+				Message: "OK",
+				Extras:  map[string]any{},
+			},
+			hasErr: false,
+		},
+		"with error in code": {
+			resp: Response{
+				Code:    0,
+				Message: "OK",
+				Extras:  map[string]any{},
+			},
+			hasErr: true,
+		},
+		"with error in message": {
+			resp: Response{
+				Code:    200,
+				Message: "",
+				Extras:  map[string]any{},
+			},
+			hasErr: true,
+		},
+		"with error in extras": {
+			resp: Response{
+				Code:    200,
+				Message: "OK",
+				Extras:  nil,
+			},
+			hasErr: true,
+		},
+	}
+
+	for testName, testCase := range tests {
+		for name, algo := range algorithms {
+			b.Run(fmt.Sprintf("%s for %s", testName, name), func(b *testing.B) {
+				benchmarkValidator(b, &testCase.resp, algo, testCase.hasErr)
+			})
+		}
+	}
 }
