ef204c9d0f72ab1d9997f9e82db102547295ecc7
Wen Junhua <wenjh1998@gmail.com>
2024-03-07 21:58:48 +0800

    Revert "fix: update lazy validator"
    This reverts commit 6b4c847d404eed4963e69d793cc0e651ee3923d5.

--- validator/lazy_validator.go
+++ validator/lazy_validator.go
@@ -1,13 +1,10 @@
 package validator
 
-import (
-	"github.com/Jh123x/go-validate/options"
-	"github.com/Jh123x/go-validate/ttypes"
-)
+import "github.com/Jh123x/go-validate/ttypes"
 
 // LazyValidator is a validator that lazily evaluates the options provided.
 type LazyValidator struct {
-	option ttypes.Validate
+	options []ttypes.Validate
 }
 
 var _ ttypes.Validator[LazyValidator] = (*LazyValidator)(nil)
@@ -22,15 +19,20 @@ func (l *LazyValidator) WithOptions(opts ...ttypes.Validate) *LazyValidator {
 	if l == nil {
 		return nil
 	}
-	combineOptions := options.And(opts...)
-	l.option = options.And(combineOptions, l.option)
-	return l
+	newValidator := *l
+	newValidator.options = append(l.options, opts...)
+	return &newValidator
 }
 
 // Validate validates the options provided.
 func (l *LazyValidator) Validate() error {
-	if l == nil || l.option == nil {
+	if l == nil {
 		return nil
 	}
-	return l.option()
+	for _, opt := range l.options {
+		if err := opt(); err != nil {
+			return err
+		}
+	}
+	return nil
 }
