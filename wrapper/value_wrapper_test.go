3980f1ad32bd7b767b04689f967198b01eb309c4
Wen Junhua <wenjh1998@gmail.com>
2024-03-18 20:43:15 +0800

    test: update tests

--- wrapper/value_wrapper_test.go
+++ wrapper/value_wrapper_test.go
@@ -1,8 +1,11 @@
 package wrapper
 
 import (
+	"fmt"
 	"testing"
 
+	"github.com/Jh123x/go-validate/errs"
+	"github.com/Jh123x/go-validate/options"
 	"github.com/Jh123x/go-validate/ttypes"
 	"github.com/stretchr/testify/assert"
 )
@@ -12,7 +15,18 @@ func TestValueWrapper(t *testing.T) {
 		value       int
 		options     ttypes.ValTest[int]
 		expectedErr error
-	}{}
+	}{
+		"success": {
+			value:       1,
+			options:     options.VWithRequire(func(v int) bool { return true }, fmt.Errorf("value is empty")),
+			expectedErr: nil,
+		},
+		"fail": {
+			value:       0,
+			options:     options.VWithRequire(func(v int) bool { return false }, fmt.Errorf("value is empty")),
+			expectedErr: fmt.Errorf("value is empty"),
+		},
+	}
 
 	for name, tc := range tests {
 		t.Run(name, func(t *testing.T) {
@@ -23,12 +37,76 @@ func TestValueWrapper(t *testing.T) {
 	}
 }
 
-func TestOptionCompatibility(t *testing.T) {
+func TestOptionCompatibility_SingleValues(t *testing.T) {
 	tests := map[string]struct {
 		value               int
 		options             ttypes.ValTest[int]
 		expectedValidateErr error
-	}{}
+	}{
+		"With Require success": {
+			value:   1,
+			options: options.VWithRequire(func(v int) bool { return true }, fmt.Errorf("value is error")),
+		},
+		"With Require fail": {
+			value:               0,
+			options:             options.VWithRequire(func(v int) bool { return false }, fmt.Errorf("value is error")),
+			expectedValidateErr: fmt.Errorf("value is error"),
+		},
+		"IsNotEmpty success": {
+			value:   1,
+			options: options.VIsNotEmpty[int](),
+		},
+		"IsNotEmpty fail": {
+			value:               0,
+			options:             options.VIsNotEmpty[int](),
+			expectedValidateErr: errs.IsNotEmptyErr,
+		},
+		"IsEmpty success": {
+			value:   0,
+			options: options.VIsEmpty[int](),
+		},
+		"IsEmpty fail": {
+			value:               1,
+			options:             options.VIsEmpty[int](),
+			expectedValidateErr: errs.IsEmptyError,
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			valueWrapper := NewValueWrapper(tc.value)
+			option := valueWrapper.WithOptions(tc.options).ToOption()
+			assert.Equal(t, tc.expectedValidateErr, option())
+		})
+	}
+}
+
+func TestOptionCompatibility_ArrValues(t *testing.T) {
+	tests := map[string]struct {
+		value               []int
+		options             ttypes.ValTest[[]int]
+		expectedValidateErr error
+	}{
+
+		"IsLength success": {
+			value:   []int{1, 2, 3},
+			options: options.VIsLength[int](1, 3),
+		},
+		"IsLength fail": {
+			value:               []int{1, 2, 3},
+			options:             options.VIsLength[int](4, 5),
+			expectedValidateErr: errs.InvalidLengthError,
+		},
+		"Contains success": {
+			value:   []int{1, 2, 3},
+			options: options.VContains(1),
+		},
+		"Contains fail": {
+			value:               []int{1, 2, 3},
+			options:             options.VContains(4),
+			expectedValidateErr: errs.ContainsError,
+		},
+	}
 
 	for name, tc := range tests {
 		t.Run(name, func(t *testing.T) {
